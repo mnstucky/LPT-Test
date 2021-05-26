766452b9d1379bd9f0511af399ff72fb08210353
Matt Stucky <mnstucky@gmail.com>
2021-05-26 17:48:55 -0500

    Link to locally hosted database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+/* Setup MySQL connection */
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'lpt',
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  connection.connect();
+  connection.query('SELECT * FROM test', function (error, results, fields) {
+    if (error) {
+      throw error;
+    } else {
+      console.log(results);
+      res.render('index', { title: 'Express' });
+    }
+  });
+  connection.end();
 });
 
 module.exports = router;
