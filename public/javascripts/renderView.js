6aa66c500cece2d5b64fcd6b5efd4fc68dcf8e1b
Matt Stucky <mnstucky@gmail.com>
2021-05-27 14:58:07 -0500

    Add pause functionality; add comments to renderView.js

--- public/javascripts/renderView.js
+++ public/javascripts/renderView.js
@@ -1,3 +1,20 @@
+// Declare module-level variables to track application state
+
+//  isPaused tracks whether the user has paused the page animation
+let isPaused = false;
+//  traceDataIndex tracks which trace the application is currently displaying
+let traceDataIndex = 0;
+
+// getTraceData calls the application's API
+// The data returned by API is an array of these objects:
+// {
+//   id: 50,
+//       powerLevels: [
+//   -52.789, -52.399, -52.439,   -54.5, -52.979, -52.869,  -54.75,
+//   ...
+//   ],
+//     time: 2019-01-17T15:30:08.000Z
+// }
 async function getTraceData() {
   try {
     const rawTraceData = await fetch('./api/');
@@ -9,10 +26,13 @@ async function getTraceData() {
   }
 }
 
+// formatDataForGoogleCharts converts API data into data consumable
+//  by the Google Charts API
 function formatDataForGoogleCharts(traceData) {
   const formattedTraceData = [];
   formattedTraceData.push(['MHz', 'dBm']);
   let currentMHz = 850;
+  // eslint-disable-next-line no-restricted-syntax
   for (const trace of traceData.powerLevels) {
     formattedTraceData.push([currentMHz, trace]);
     currentMHz += (1 / 2);
@@ -20,7 +40,9 @@ function formatDataForGoogleCharts(traceData) {
   return formattedTraceData;
 }
 
+// drawChart renders a single chart from the inputted data and timestamp
 function drawChart(formattedTraceData, timeStamp) {
+  // eslint-disable-next-line no-undef
   const dataToDraw = google.visualization.arrayToDataTable(formattedTraceData);
   const options = {
     title: timeStamp,
@@ -45,16 +67,20 @@ function drawChart(formattedTraceData, timeStamp) {
       position: 'none',
     },
   };
+  // eslint-disable-next-line no-undef
   const chart = new google.visualization.LineChart(document.getElementById('chart'));
   chart.draw(dataToDraw, options);
 }
 
+// sleep pauses program execution for the specified number of milliseconds
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-async function startAnimation(traceData, traceDataIndex) {
-  while (true) {
+// startAnimation receives the entire block of API data and renders a series of charts,
+//  with the current chart refreshing every one second
+async function startAnimation(traceData) {
+  while (!isPaused) {
     const formattedTraceData = formatDataForGoogleCharts(traceData[traceDataIndex]);
     drawChart(formattedTraceData, traceData[traceDataIndex].time);
     if (traceDataIndex < traceData.length - 1) {
@@ -62,14 +88,34 @@ async function startAnimation(traceData, traceDataIndex) {
     } else {
       traceDataIndex = 0;
     }
+    // eslint-disable-next-line no-await-in-loop
     await sleep(1000);
   }
 }
 
+// handlePauseButton starts and stop the chart animation upon a click
+function handlePauseButton(event, traceData) {
+  const pauseButton = document.getElementById('pause');
+  if (!isPaused) {
+    isPaused = true;
+    pauseButton.textContent = 'Resume';
+  } else {
+    isPaused = false;
+    pauseButton.textContent = 'Pause';
+    startAnimation(traceData, traceDataIndex);
+  }
+}
+
+// Call functions and declare event listeners to start the application
 document.addEventListener('readystatechange', async (event) => {
   if (event.target.readyState === 'complete') {
+    // eslint-disable-next-line no-undef
     await google.charts.load('current', { packages: ['corechart'] });
     const traceData = await getTraceData();
+    const pauseButton = document.getElementById('pause');
+    pauseButton.addEventListener('click', (buttonEvent) => {
+      handlePauseButton(buttonEvent, traceData);
+    });
     if (traceData) {
       startAnimation(traceData, 0);
     } else {
